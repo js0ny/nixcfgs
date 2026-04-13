8ebddcb5bfe86366315254427cfed53d8682060f
js0ny <contact@js0ny.net>
2026-04-13 15:03:28 +0100

    chore(hm/obsidian): remove references

--- home/programs/productivity/obsidian/default.nix
+++ home/programs/productivity/obsidian/default.nix
@@ -1,25 +1,8 @@
 {
   pkgs,
-  lib,
   ...
 }:
-let
-in
 {
-  imports = [
-    ./vimrc-support.nix
-    ./vim-im-select.nix
-  ];
-  programs.obsidian = {
-    enable = false;
-    cli.enable = true;
-    vaults."HomeManagerDryRun" = {
-      enable = true;
-      target = "HomeManagerDryRun"; # relative to $HOME
-      settings = {
-      };
-    };
-  };
   home.packages = with pkgs; [
     obsidian
   ];
--- home/programs/productivity/obsidian/vimrc-support.nix
+++
@@ -1,9 +0,0 @@
-{ ... }:
-{
-  programs.obsidian.defaultSettings = {
-    extraFiles.".obsidian.vimrc" = {
-      target = "../.obsidian.vimrc";
-      source = ./obsidian.vim;
-    };
-  };
-}
