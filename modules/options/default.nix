e1834acbb60b6a9a7cdadb6b0d7fddb955700d30
js0ny <contact@js0ny.net>
2026-04-21 18:20:50 +0100

    refactor(modules): subdir of nixdots

--- modules/options/default.nix
+++ modules/options/default.nix
@@ -1,15 +1,6 @@
 { ... }:
 {
   imports = [
-    ./core.nix
-    ./style.nix
-    ./laptop.nix
-    ./programs.nix
-    ./sops.nix
-    ./desktop.nix
-    ./machine.nix
-    ./virtualisation.nix
-    ./impermanence.nix
-    ./server.nix
+    ./nixdots
   ];
 }
---
+++ modules/options/nixdots/default.nix
@@ -0,0 +1,15 @@
+{ ... }:
+{
+  imports = [
+    ./core.nix
+    ./style.nix
+    ./laptop.nix
+    ./programs.nix
+    ./sops.nix
+    ./desktop.nix
+    ./machine.nix
+    ./virtualisation.nix
+    ./impermanence.nix
+    ./server.nix
+  ];
+}
