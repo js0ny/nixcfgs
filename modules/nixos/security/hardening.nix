963d85069f91e0a30a8de733d62d197b07927f1a
js0ny <contact@js0ny.net>
2026-05-08 07:37:14 +0100

    fix(nixos/hardening): dirty-frag

--- modules/nixos/security/hardening.nix
+++ modules/nixos/security/hardening.nix
@@ -1,8 +1,23 @@
 { lib, pkgs, ... }:
 {
-  # https://copy.fail
-  boot.extraModprobeConfig = ''
-    blacklist algif_aead
-    install algif_aead ${lib.getExe' pkgs.coreutils "false"}
-  '';
+  boot.extraModprobeConfig =
+    # https://copy.fail
+    ''
+      blacklist algif_aead
+      install algif_aead ${lib.getExe' pkgs.coreutils "false"}
+    ''
+    # https://github.com/V4bel/dirtyfrag
+    ++ ''
+      install esp4 ${lib.getExe' pkgs.coreutils "false"}
+      install esp6 ${lib.getExe' pkgs.coreutils "false"}
+      install rxrpc ${lib.getExe' pkgs.coreutils "false"}
+    '';
+
+  # https://github.com/V4bel/dirtyfrag
+  # IPSec and AFS
+  boot.blacklistedKernelModules = [
+    "esp4"
+    "esp6"
+    "rxrpc"
+  ];
 }
