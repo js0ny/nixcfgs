923065eb573c20714ff39e07fee3ec2d71e6923b
js0ny <contact@js0ny.net>
2026-06-28 09:47:41 +0100

    fix: get bauhaus to build

--- modules/home/desktop-base.nix
+++ modules/home/desktop-base.nix
@@ -1,44 +1,19 @@
 {
   lib,
   config,
+  myLib,
   ...
 }:
 {
   imports = [
     # keep-sorted start
 
-    ../../modules/desktop/home/niri/module.nix
-    ../../modules/home/programs/aichat.nix
-    # ../../modules/home/programs/walker.nix
-    ../../modules/home/programs/browsers/url-dispatcher.nix
-    ../../modules/home/programs/editors/vscode/module.nix
-    # ../../modules/home/programs/fsearch.nix # use vicinae
-    ../../modules/home/programs/loupe.nix
-    ../../modules/home/programs/media/feishin.nix
-    ../../modules/home/programs/media/mpv.nix
-    ../../modules/home/programs/mime.nix
-    ../../modules/home/programs/miniserve.nix
-    ../../modules/home/programs/nix-index.nix
-    ../../modules/home/programs/pcloud.nix
-    ../../modules/home/programs/pim.nix
-    ../../modules/home/programs/productivity/anki.nix
-    ../../modules/home/programs/productivity/obsidian/module.nix
-    ../../modules/home/programs/productivity/readest.nix
-    ../../modules/home/programs/productivity/sdcv.nix
-    ../../modules/home/programs/productivity/sioyek/module.nix
-    ../../modules/home/programs/sandboxed.nix
-    ../../modules/home/programs/shell/direnv.nix
-    ../../modules/home/programs/social/discord.nix
-    ../../modules/home/programs/social/qq.nix
-    ../../modules/home/programs/social/telegram.nix
-    ../../modules/home/programs/social/wechat.nix
-    ../../modules/home/programs/terminals/kitty.nix
-    ../../modules/home/programs/terminals/tmux.nix
-    ../../modules/programs/gaming/emulators/retroarch.nix
-    ../modules/packages/devtools.nix
-    ../modules/packages/flatpak.nix
-    ../modules/packages/gui.nix
     ./linux-base.nix
+    ../../modules/desktop/home/niri/module.nix
+    ../../modules/packages/devtools.nix
+    ../../modules/packages/flatpak.nix
+    ../../modules/packages/gui.nix
+    ../../modules/programs/gaming/emulators/retroarch.nix
     # keep-sorted end
   ];
 
@@ -53,9 +28,7 @@
     hardware.enable = true;
   };
 
-  # Hidden directories under $HOME
   home.file.".hidden".text = lib.concatStringsSep "\n" [
     "PDX"
   ];
-
 }
--- modules/home/desktop-extra.nix
+++ modules/home/desktop-extra.nix
@@ -9,10 +9,26 @@
   imports = [
     # keep-sorted start
 
+    ./desktop-base.nix
     ../../modules/desktop/home/niri/module.nix
-    ../../modules/home/programs/bottles.nix
-    ../../modules/home/programs/browsers/qutebrowser.nix
-    ../../modules/home/programs/cherry-studio.nix
-  ]
-  ;
+    ../../modules/programs/gaming/celeste/module.nix
+    ../../modules/programs/gaming/emulators/cemu.nix
+    ../../modules/programs/gaming/emulators/retroarch.nix
+    ../../modules/programs/gaming/emulators/ryujinx.nix
+    ../../modules/programs/gaming/minecraft.nix
+    ../../modules/packages/extra.nix
+    ../../modules/packages/flatpak.nix
+    # keep-sorted end
+  ];
+
+  programs.pdf2zh.enable = true;
+
+  catppuccin.thunderbird.profile = config.home.username;
+
+  nixdots.sops.secrets = {
+    nix_github_pat = {
+      env = [ "NIX_CONFIG" ];
+      sopsFile = secrets + /hosts.yaml;
+    };
+  };
 }
