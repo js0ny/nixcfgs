2266dd7b1417229c67f4a59ef50bde477e4aa23b
js0ny <contact@js0ny.net>
2026-09-16 19:04:15 +0800

    feat(codex): chatgpt-desktop config

--- modules/programs/vibe-coding/codex.nix
+++ modules/programs/vibe-coding/codex.nix
@@ -46,7 +46,7 @@ in
       check_for_update_on_startup = false;
       default_permissions = ":workspace";
       sandbox_mode = "danger-full-access";
-      model = "gpt-5.6-sol";
+      model = "gpt-6-astra";
       features.hooks = true;
       tui = {
         status_line = [
@@ -61,6 +61,69 @@ in
         ];
         status_line_use_colors = true;
         vim_mode_default = true;
+      };
+      # Hash calculation /* python3 */
+      /*
+        import hashlib
+        import json
+
+        identity = {
+            "event_name": "session_start",
+            "hooks": [{
+                "async": False,
+                "command": "bash '/home/js0ny/.config/codex/herdr-agent-state.sh' session",
+                "timeout": 10,
+                "type": "command",
+            }],
+        }
+
+        payload = json.dumps(identity, sort_keys=True, separators=(",", ":"), ensure_ascii=False)
+        print("sha256:" + hashlib.sha256(payload.encode("utf-8")).hexdigest())
+      */
+      hooks.state = {
+        "${config.xdg.configHome}/codex/hooks.json:session_start:0:0" = {
+          trusted_hash = "sha256:abcdb76f675d626b427d709a097643c97288c1ec6bf4dcbb3fb96bc7f874e8ba";
+        };
+      };
+      desktop = {
+        followUpQueueMode = "queue";
+        localeOverride = "zh-CN";
+        show-context-window-usage = true;
+        notifications-turn-mode = "unfocused";
+        appearanceLightCodeThemeId = "catppuccin";
+        appearanceDarkCodeThemeId = "catppuccin";
+        usePointerCursors = true;
+        appearanceDiffMarkerStyle = "color";
+        browser-show-full-url = true;
+        keepRemoteControlAwakeWhilePluggedIn = false;
+        open-in-target-preferences.global = "ghostty";
+        # Catppuccin
+        appearanceLightChromeTheme = {
+          accent = "#8839ef";
+          accentSource = "custom";
+          contrast = 45;
+          ink = "#4c4f69";
+          opaqueWindows = false;
+          surface = "#eff1f5";
+          semanticColors = {
+            diffAdded = "#40a02b";
+            diffRemoved = "#d20f39";
+            skill = "#8839ef";
+          };
+        };
+        appearanceDarkChromeTheme = {
+          accent = "#cba6f7";
+          accentSource = "custom";
+          contrast = 60;
+          ink = "#cdd6f4";
+          opaqueWindows = false;
+          surface = "#1e1e2e";
+          semanticColors = {
+            diffAdded = "#a6e3a1";
+            diffRemoved = "#f38ba8";
+            skill = "#cba6f7";
+          };
+        };
       };
     };
   };
