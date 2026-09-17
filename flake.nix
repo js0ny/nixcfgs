4d5e8ee2464398e7e95cd28115362d7e93d06e71
js0ny <contact@js0ny.net>
2026-09-17 11:56:02 +0800

    chore(flake): drop sfd-nix

--- flake.nix
+++ flake.nix
@@ -60,9 +60,6 @@
 
     # {{{ Application Supports
     flatpak-nix.url = "github:gmodena/nix-flatpak";
-    sfd-nix = {
-      url = "git+https://forge.asnk.io/sugar/sfd-nix";
-    };
     glide-browser = {
       url = "github:glide-browser/glide.nix";
       inputs.nixpkgs.follows = "nixpkgs";
